693bc791b48ea384a351b7cd8f743892db9754b8
LuanAmaral <luan.rocha.amaral@gmail.com>
2025-06-05 10:30:47 -0300

    add tensor conversion and arithmetic operation for State class

--- tools/mpc_tools.py
+++ tools/mpc_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 class State:
     def __init__(self, x=0.0, y=0.0, psi=0.0, v=0.0, delta=0.0):
@@ -18,6 +19,41 @@ class State:
         
     def __repr__(self):
         return f"State(X={self.x}, Y={self.y}, psi={self.psi}, v={self.v}, delta={self.delta})"
+    
+    def to_tensor(self):
+        """
+        Convert the state to a PyTorch tensor.
+        :return: Tensor representation of the state.
+        """
+        return torch.tensor([self.x, self.y, self.psi, self.v, self.delta], dtype=torch.float32)
+    
+    def __add__(self, other: 'State') -> 'State':
+        """
+        Add two states together.
+        :param other: Another State object.
+        :return: New State object with summed attributes.
+        """
+        return State(
+            x=self.x + other.x,
+            y=self.y + other.y,
+            psi=self.psi + other.psi,
+            v=self.v + other.v,
+            delta=self.delta + other.delta
+        )
+        
+    def __sub__(self, other: 'State') -> 'State':
+        """
+        Subtract another state from this state.
+        :param other: Another State object.
+        :return: New State object with subtracted attributes.
+        """
+        return State(
+            x=self.x - other.x,
+            y=self.y - other.y,
+            psi=self.psi - other.psi,
+            v=self.v - other.v,
+            delta=self.delta - other.delta
+        )
         
 class Inputs:
     def __init__(self, acc=0.0, steering_vel=0.0):
